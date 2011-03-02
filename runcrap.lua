fed90195fb754264cfed8dd12f3f3f8c471e1006
DMB <netsdanita@yahoo.com.au>
2011-03-03 07:55:03 +1100

    created 'runcrap.lua'
    Eventually machine will be a runable module that can
    do what 'runcrap' can, and more. but for now its useful.

---
+++ runcrap.lua
@@ -0,0 +1,22 @@
+require 'machine'
+require'asm'
+
+m=machine:new()
+
+local file = arg[1]
+
+assert(file, "what the? I've got nothing here!")
+
+if file:match(".*.asm") then
+	chk= asm.load(file, true)
+	chk=asm.parse(chk)
+	m:load(chk)
+elseif file:match(".*.crap") then
+	local file, err = io.open(file)
+	assert(file, err)
+	chk = file:read("*a")
+	m:load(chk)
+end
+
+m:run()
+
