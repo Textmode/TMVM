75cd20470c4e448aad2320305f339691e6ec3b03
DMB <netsdanita@yahoo.com.au>
2011-03-02 15:35:58 +1100

    Created 'mnz-test.asm'

---
+++ examples/mnz-test.asm
@@ -0,0 +1,17 @@
+; set A = 1, B=42
+MOV 42, A
+SWP A, B
+MOV 1, A
+; if A!=0 then Move B to &100
+MNZ A, B, 100
+
+MOV 0, A
+; if A!=0 then Move B to &111
+MNZ A, B, 100
+
+; show the results
+MOV &100, A
+SHW A
+MOV &111, A
+SHW A
+
